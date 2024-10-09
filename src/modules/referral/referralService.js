d09a8790e6e9714801f049dfa0a24b67989f2a5d
aunghp <aunghp.io@gmail.com>
2024-10-10 01:25:52 +0630

    update

--- src/modules/referral/referralService.js
+++ src/modules/referral/referralService.js
@@ -1,6 +1,7 @@
 import { endpoints } from "../../constants/endpoints";
 import { getRequest, postRequest } from "../../helpers/api";
 import { httpServiceHandler } from "../../helpers/handler";
+import { setReferralIndex } from "./referralSlice";
 
 export const referralService = {
     check: async (dispatch, referral) => {
@@ -12,6 +13,18 @@ export const referralService = {
     store: async (dispatch, payload) => {
         const result = await postRequest(`${endpoints.referral}`, payload);
         await httpServiceHandler(dispatch, result);
+        return result;
+    },
+
+    index: async (dispatch) => {
+        const result = await getRequest(endpoints.referral);
+        await httpServiceHandler(dispatch, result);
+
+        if(result.status === 200) {
+            console.log(result);
+            dispatch(setReferralIndex(result.data.data ? result.data.data : result.data));
+        }
+
         return result;
     }
 }
--- src/modules/referral/view/ReferralList.js
+++ src/modules/referral/view/ReferralList.js
@@ -1,72 +1,92 @@
+import { useCallback, useEffect, useState } from "react"
 import { Header } from "../../../shares/Header"
 import { Notification } from "../../../shares/Notification"
 import { CreateReferralLink } from "../entry/CreateReferralLink"
+import { useDispatch, useSelector } from "react-redux"
+import { referralService } from "../referralService"
+import { paths } from "../../../constants/paths"
+import { useNavigate } from "react-router-dom"
+import moment from "moment"
 
 export const ReferralList = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    const { user } = useSelector(state => state.account);
+    const { referrals } = useSelector(state => state.referral);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    /** Loading Referral Data */
+    const mount = useCallback(async () => {
+        setLoading(true);
+        await referralService.index(dispatch);
+        setLoading(false);
+    }, [dispatch]);
+
+    /** Loading initialize data */
+    useEffect(() => {
+        mount();
+    }, [mount]);
 
     return (
         <>
             <Header />
 
             <div className="container-fluid">
-                <div className="row">
-                    <Notification />
+                {user && user.agent_type === 'MAIN_AGENT' && (
+                    <div className="row">
+                        <Notification />
 
-                    <div className="col-12 mt-3">
-                        <div className="row">
-                            <div className="col-12 mt-3">
-                                <CreateReferralLink />
-                            </div>
+                        <div className="col-12 mt-3">
+                            <div className="row">
+                                <div className="col-12 mt-3">
+                                    <CreateReferralLink />
+                                </div>
 
-                            <div className="col-12 col-md-12 mt-3">
-                                <div className="card" style={{ background: "#212529", color: "#fff" }}>
-                                    <div className="card-body">
-                                        <div className="row">
-                                            <div className="col-12 mt-3">
-                                                <h4> Your Referral Link </h4>
-                                            </div>
+                                <div className="col-12 col-md-12 mt-3">
+                                    <div className="card" style={{ background: "#212529", color: "#fff" }}>
+                                        <div className="card-body">
+                                            <div className="row">
+                                                <div className="col-12 mt-3">
+                                                    <h4> Your Referral Link </h4>
+                                                </div>
 
-                                            <div className="col-12 mt-3">
-                                                <div className="table-responsive">
-                                                    <table className="table table-sm table-dark">
-                                                        <thead>
-                                                            <tr className="agent-list-table-title">
-                                                                <th scope="col"> # </th>
-                                                                <th scope="col"> ID</th>
-                                                                <th scope="col"> Deposit <small> (Kyats)</small> </th>
-                                                                <th scope="col"> ROI <small> (Kyats) </small> </th>
-                                                                <th scope="col"> Deposit Date </th>
-                                                                <th scope="col"> Expired Date </th>
-                                                                <th scope="col"> Status </th>
-                                                            </tr>
-                                                        </thead>
-{/* 
-                                                        <tbody className="agent-list-table-row">
-                                                            {!loading && deposits && deposits.map((deposit, index) => {
-                                                                return (
-                                                                    <tr key={`deposit_id_${index}`} style={{ width: "100%" }}>
-                                                                        <td> {index + 1} </td>
-                                                                        <td
-                                                                            style={{
-                                                                                textDecoration: "underline #d3d3d3",
-                                                                                cursor: "pointer",
-                                                                            }}
-                                                                            onClick={() => navigate(`${paths.repayment}/deposit/${deposit.id}`)}
-                                                                        >
-                                                                            {deposit.id}
-                                                                        </td>
-                                                                        <td> {numeral(deposit.deposit_amount).format('0,0')} </td>
-                                                                        <td> {numeral(deposit.roi_amount).format('0,0')} </td>
-                                                                        <td> {moment(deposit.created_at).format("DD/MM/YYYY hh:mm:ss A")} </td>
-                                                                        <td> {moment(deposit.expired_at).format("DD/MM/YYYY")} </td>
-                                                                        <td> {moment(deposit.expired_at).isBefore(moment()) ?
-                                                                            <Badge bg="danger"> EXPIRED </Badge> :
-                                                                            <Badge bg="success"> ACTIVE </Badge>} </td>
-                                                                    </tr>
-                                                                )
-                                                            })}
-                                                        </tbody> */}
-                                                    </table>
+                                                <div className="col-12 mt-3">
+                                                    <div className="table-responsive">
+                                                        <table className="table table-sm table-dark">
+                                                            <thead>
+                                                                <tr className="agent-list-table-title">
+                                                                    <th scope="col"> Link </th>
+                                                                    <th scope="col"> Count </th>
+                                                                    <th scope="col"> Commission (%) </th>
+                                                                    <th scope="col"> Expired Date </th>
+                                                                </tr>
+                                                            </thead>
+
+                                                            <tbody className="agent-list-table-row">
+                                                                {!loading && referrals && referrals.map((referral, index) => {
+                                                                    return (
+                                                                        <tr key={`referral_id_${index}`} style={{ width: "100%" }}>
+                                                                            <td
+                                                                                style={{
+                                                                                    textDecoration: "underline #d3d3d3",
+                                                                                    cursor: "pointer",
+                                                                                }}
+                                                                                onClick={() => navigate(`${paths.referral}/${referral.id}`)}
+                                                                            >
+                                                                                {referral.id}
+                                                                            </td>
+                                                                            <td> {referral.count} </td>
+                                                                            <td> {referral.commission} </td>
+                                                                            <td> {moment(referral.expired_at).format("DD/MM/YYYY")} </td>
+                                                                        </tr>
+                                                                    )
+                                                                })}
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -75,7 +95,7 @@ export const ReferralList = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </>
     )
